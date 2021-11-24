19468614b026ee6479442e1de3a35bd06e1f1a58
Sveniorita <92942919+Sveniorita@users.noreply.github.com>
2021-11-24 12:02:18 +0100

    Hausaufgaben 24.11.21

---
+++ Assets/Scripts/WaveScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveScript : MonoBehaviour
+{
+
+    // Arme initialisieren
+    public GameObject NormalArm;
+    public GameObject WaveArm;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Normaler Arm wird ausgeblendet, solange der Knopf gedrückt ist. 
+      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NormalArm.SetActive(false);
+        };
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            NormalArm.SetActive(true);
+        };
+
+
+
+        //Winkender Arm wird angezeigt, solange der Knopf gedrückt ist. 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            WaveArm.SetActive(true);
+        };
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            WaveArm.SetActive(false);
+        };
+
+
+    }
+
+    
+
+}
